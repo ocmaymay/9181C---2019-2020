54813dee43803bf07817645096fa0aa108ae474a
ocmaymay <61607864+ocmaymay@users.noreply.github.com>
2020-02-29 01:12:32 -0800

    Add files via upload
    Heritage Woods / Enver Creek

---
+++ Herritage/B4Herritage/src/main.cpp
@@ -0,0 +1,348 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// LeftMotor            motor         12              
+// LeftMotor2           motor         16              
+// RightMotor           motor         10              
+// RightMotor2          motor         8               
+// RightPusher          motor         6               
+// IntakeR              motor         15              
+// IntakeL              motor         3               
+// Controller1          controller                    
+// Arm                  motor         1               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include  <string>
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  LeftMotor.setStopping(vex::brakeType::brake);
+  LeftMotor2.setStopping(vex::brakeType::brake);
+  RightMotor.setStopping(vex::brakeType::brake);
+  RightMotor2.setStopping(vex::brakeType::brake);
+  RightPusher.setStopping(vex::brakeType::brake);
+  IntakeL.setStopping(brake);
+  IntakeR.setStopping(brake);
+  Arm.setStopping(hold);
+  IntakeL.setMaxTorque(95, percent);
+  IntakeR.setMaxTorque(95, percent);
+  RightPusher.setMaxTorque(95, percent);
+  Arm.setMaxTorque(95, percent);
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+
+static void stopAllDrive(void){
+  LeftMotor.stop();
+  LeftMotor2.stop();
+  RightMotor.stop();
+  RightMotor2.stop();
+}
+
+static void stopIntake(void){
+  IntakeL.stop();
+  IntakeR.stop();
+}
+
+static void intakeSpdChange(int spd, int fB)
+{
+ IntakeL.setVelocity(spd,percent);
+ IntakeR.setVelocity(spd,percent); 
+ if(fB == 1){
+ IntakeL.spin(forward);
+ IntakeR.spin(forward);
+ } 
+ else{
+ IntakeL.spin(reverse);
+ IntakeR.spin(reverse);
+ }
+}
+
+int onoff(void){
+  int x;
+  if(Controller1.ButtonR1.pressing()){
+    x=65;
+  }
+  else if (Controller1.ButtonL1.pressing()) {
+    x=-80;
+  }
+  else {
+  x=0;
+  }
+     return x; 
+};
+
+int upDwn(void){
+  int x;
+    if(Controller1.ButtonX.pressing()){
+    x=30;
+  }
+  else if (Controller1.ButtonB.pressing()) {
+    x=-50;
+  }
+  else {
+  x=0;
+  }
+     return x; 
+}
+
+int armFunc(void){
+  int x;
+    if(Controller1.ButtonR2.pressing()){
+    x=90;
+  }
+  else if (Controller1.ButtonL2.pressing()) {
+    x=-90;
+  }
+  else {
+  x=0;
+  }
+     return x; 
+}
+
+static void moveWithSeconds(int velR, int velL, int msecs){
+  LeftMotor.setVelocity(velL,percent);
+  LeftMotor2.setVelocity(velL,percent);
+  RightMotor.setVelocity(velR,percent);
+  RightMotor2.setVelocity(velR,percent);
+  LeftMotor.spin(fwd);
+  LeftMotor2.spin(fwd);
+  RightMotor.spin(fwd);
+  RightMotor2.spin(fwd);
+  wait(msecs,msec);
+  stopAllDrive();
+}
+
+static void moveWithRotations(int velR, int velL, int degsR, int degsL){
+  LeftMotor.setVelocity(velL,percent);
+  LeftMotor2.setVelocity(velL,percent);
+  RightMotor.setVelocity(velR,percent);
+  RightMotor2.setVelocity(velR,percent);
+  LeftMotor.startRotateFor(degsL,degrees);
+  LeftMotor2.startRotateFor(degsL,degrees);
+  RightMotor.startRotateFor(degsR,degrees);
+  RightMotor2.startRotateFor(degsR,degrees);
+  waitUntil(LeftMotor.isDone()&&RightMotor.isDone());
+  stopAllDrive();
+}
+
+static void deployCubes(void){
+  LeftMotor.setStopping(vex::brakeType::hold);
+  LeftMotor2.setStopping(vex::brakeType::hold);
+  RightMotor.setStopping(vex::brakeType::hold);
+  RightMotor2.setStopping(vex::brakeType::hold);
+  RightPusher.setStopping(coast);
+  stopAllDrive();
+  RightPusher.setVelocity(35,pct);
+  RightPusher.spinFor(2.5,seconds);
+  RightPusher.setStopping(hold);
+  RightPusher.stop();
+  wait(300,msec);
+  moveWithSeconds(7,7,600);
+  wait(300,msec);
+  moveWithSeconds(-30,-30,700);
+  RightPusher.setVelocity(-70,pct);
+  RightPusher.spinFor(1,seconds);
+  LeftMotor.setStopping(vex::brakeType::brake);
+  LeftMotor2.setStopping(vex::brakeType::brake);
+  RightMotor.setStopping(vex::brakeType::brake);
+  RightMotor2.setStopping(vex::brakeType::brake);
+  }
+
+  static void driveStraight(void){
+      LeftMotor.spin(vex::directionType::fwd,
+                     (Controller1.Axis3.position()/1.2),
+                     vex::velocityUnits::pct);
+      LeftMotor2.spin(vex::directionType::fwd,
+                      (Controller1.Axis3.position()/1.2),
+                      vex::velocityUnits::pct);
+      RightMotor.spin(vex::directionType::fwd,
+                      (Controller1.Axis3.position()/1.2),
+                      vex::velocityUnits::pct);
+      RightMotor2.spin(vex::directionType::fwd,
+                       (Controller1.Axis3.position()/1.2),
+                       vex::velocityUnits::pct);
+  }
+
+
+  static void driving(double div){
+      LeftMotor.spin(
+          vex::directionType::fwd,
+          ((Controller1.Axis3.position() + Controller1.Axis4.position()) / div),
+          vex::velocityUnits::pct);
+      LeftMotor2.spin(
+          vex::directionType::fwd,
+          ((Controller1.Axis3.position() + Controller1.Axis4.position()) / div),
+          vex::velocityUnits::pct);
+      RightMotor.spin(
+          vex::directionType::fwd,
+          ((Controller1.Axis3.position() - Controller1.Axis4.position()) / div),
+          vex::velocityUnits::pct);
+      RightMotor2.spin(
+          vex::directionType::fwd,
+          ((Controller1.Axis3.position() - Controller1.Axis4.position()) / div),
+          vex::velocityUnits::pct);
+  }
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+void autonomous(void) {
+pre_auton();
+intakeSpdChange(70, 1);
+moveWithRotations(30,30,550,550);
+wait(200,msec);
+moveWithRotations(20,20,280,-280);
+wait(200,msec);
+moveWithRotations(40,40,350,350);
+moveWithRotations(30,30,-300,-300);
+wait(200,msec);
+moveWithRotations(40,40,510,-510);
+wait(300,msec);
+moveWithRotations(60,60,350,350);
+moveWithRotations(30,30,200,200);
+moveWithRotations(30,30,-100,100);
+wait(300,msec);
+intakeSpdChange(20, 0);
+moveWithRotations(50,50,350,350);
+stopIntake();
+wait(200,msec);
+//hardcode deploycubes to prevent tipping stack, changing pshing value
+  LeftMotor.setStopping(vex::brakeType::hold);
+  LeftMotor2.setStopping(vex::brakeType::hold);
+  RightMotor.setStopping(vex::brakeType::hold);
+  RightMotor2.setStopping(vex::brakeType::hold);
+  stopAllDrive();
+  IntakeL.spin(forward);
+  IntakeR.spin(forward);
+  RightPusher.setVelocity(60,pct);
+  RightPusher.rotateFor(160,deg);
+  RightPusher.setVelocity(40,pct);
+  RightPusher.rotateFor(100,deg);
+  RightPusher.setVelocity(20,pct);
+  RightPusher.spinFor(1.1,seconds);
+  RightPusher.stop();
+  wait(300,msec);
+  IntakeL.setVelocity(-50, percent);
+  IntakeR.setVelocity(-50, percent);
+  moveWithSeconds(10,10,500);
+  wait(500,msec);
+  IntakeL.setVelocity(-25, percent);
+  IntakeR.setVelocity(-25, percent);
+  RightPusher.setVelocity(-70,pct);
+  RightPusher.spinFor(1,seconds);
+  moveWithSeconds(-30,-30,700);
+  IntakeL.stop();
+  IntakeR.stop();
+  LeftMotor.setStopping(vex::brakeType::brake);
+  LeftMotor2.setStopping(vex::brakeType::brake);
+  RightMotor.setStopping(vex::brakeType::brake);
+  RightMotor2.setStopping(vex::brakeType::brake);
+}
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    short yee = abs(Controller1.Axis4.position());
+    intakeSpdChange(onoff(), 1);
+    if (yee <= 15) {
+      driveStraight();
+    }
+    else {
+      driving(1.4);
+    }
+    if (Controller1.ButtonA.pressing()){
+      deployCubes();
+    }
+
+  //Pusher
+    if(Controller1.ButtonX.pressing()||Controller1.ButtonB.pressing()){
+      RightPusher.setVelocity(upDwn(), percent);
+      RightPusher.spin(fwd);
+    }
+    else {
+      RightPusher.stop();
+    }
+    //Arm
+    if (Controller1.ButtonA.pressing()){
+      deployCubes();
+    }
+    Arm.setVelocity(Controller1.Axis2.position()/1.3, percent);
+    Arm.spin(fwd);
+
+    /*if(Controller1.ButtonR2.pressing()||Controller1.ButtonL2.pressing()){
+      Arm.setVelocity(armFunc(), percent);
+      Arm.spin(fwd);
+    }
+    else {
+      Arm.stop();
+    }
+    */
+    vex::task::sleep(5); // Sleep the task for a short amount of time to
+                          // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
---
+++ Herritage/R4Herritage/src/robot-config.cpp
@@ -0,0 +1,54 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftMotor = motor(PORT12, ratio18_1, false);
+motor LeftMotor2 = motor(PORT16, ratio18_1, false);
+motor RightMotor = motor(PORT10, ratio18_1, true);
+motor RightMotor2 = motor(PORT8, ratio18_1, true);
+motor RightPusher = motor(PORT6, ratio36_1, true);
+motor IntakeR = motor(PORT15, ratio18_1, true);
+motor IntakeL = motor(PORT3, ratio18_1, false);
+motor Arm = motor(PORT1, ratio18_1, false);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variables used for controlling motors based on controller inputs
+bool Controller1XBButtonsControlMotorsStopped = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_callback_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    // check the X/B buttons status to control LeftMotor
+    if (Controller1.ButtonX.pressing()) {
+      LeftMotor.spin(forward);
+      Controller1XBButtonsControlMotorsStopped = false;
+    } else if (Controller1.ButtonB.pressing()) {
+      LeftMotor.spin(reverse);
+      Controller1XBButtonsControlMotorsStopped = false;
+    } else if (!Controller1XBButtonsControlMotorsStopped){
+      LeftMotor.stop();
+      Controller1XBButtonsControlMotorsStopped = true;
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_callback_Controller1);
+}
